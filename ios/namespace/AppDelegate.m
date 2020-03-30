ad60c2596034bdcb03ea31f4d0a4e51f548017fe
Ishaq Hassan <ishaquehassan@gmail.com>
2020-03-30 19:31:53 +0500

    Setup dependencies done along with Google Maps

--- ios/namespace/AppDelegate.m
+++ ios/namespace/AppDelegate.m
@@ -11,6 +11,7 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
@@ -31,6 +32,7 @@ static void InitializeFlipper(UIApplication *application) {
   InitializeFlipper(application);
 #endif
 
+  [GMSServices provideAPIKey:@"AIzaSyBt4vMDVPZTcNVaJg5tInjLk9PZ-WWfrOc"];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"namespace"
